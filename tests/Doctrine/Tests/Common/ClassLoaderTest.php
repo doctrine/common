f46cc40065ec5aadeff00917770018343fd3eeb6
Martin <mpoehlmann@songpier.com>
2011-11-16 12:18:40 +0100

    Testing scripts that fail with current implementation

--- tests/Doctrine/Tests/Common/ClassLoaderTest.php
+++ tests/Doctrine/Tests/Common/ClassLoaderTest.php
@@ -25,12 +25,25 @@ class ClassLoaderTest extends \Doctrine\Tests\DoctrineTestCase
     public function testClassExists()
     {
         $this->assertFalse(ClassLoader::classExists('ClassLoaderTest\ClassD'));
+        
+        // class loader with return true statement on success
         $badLoader = function($className) {
             require __DIR__ . '/ClassLoaderTest/ClassD.php';
             return true;
         };
         spl_autoload_register($badLoader);
         $this->assertTrue(ClassLoader::classExists('ClassLoaderTest\ClassD'));
+        spl_autoload_unregister($badLoader);
+        
+        $this->assertFalse(ClassLoader::classExists('ClassLoaderTest\ClassE'));
+        
+        // test class loader without return true statement on success like symfony (and probably many others)
+        $badLoader = function($className) {
+            require_once __DIR__ . '/ClassLoaderTest/ClassE.php';
+            // no return true
+        };
+        spl_autoload_register($badLoader);
+        $this->assertTrue(ClassLoader::classExists('ClassLoaderTest\ClassE')); // Fails w/o fix
         spl_autoload_unregister($badLoader);
     }
 
@@ -41,5 +54,25 @@ class ClassLoaderTest extends \Doctrine\Tests\DoctrineTestCase
         $this->assertTrue(ClassLoader::getClassLoader('ClassLoaderTest\ClassD') instanceof \Doctrine\Common\ClassLoader);
         $this->assertNull(ClassLoader::getClassLoader('This\Class\Does\Not\Exist'));
         $cl->unregister();
+    }
+    
+    
+    public function testClassExistsMultipleLoaders()
+    {
+        // sometimes you may want to split source directories (eg, src, test-src)
+        // but the namespaces are the same for bot...
+        $classLoader1 = new ClassLoader('NS', __DIR__.'/ClassLoaderTest2/src1');
+        $classLoader1->register();
+    
+        $classLoader2 = new ClassLoader('NS', __DIR__.'/ClassLoaderTest2/src2');
+        $classLoader2->register();
+    
+        $this->assertTrue(ClassLoader::classExists('NS\\Class1'));
+        new \NS\Class1();
+        $this->assertTrue(ClassLoader::classExists('NS\\Class2'));
+        new \NS\Class2(); // fatal error (file not found) even if loadable via classLoader2
+        
+        $classLoader1->unregister();
+        $classLoader2->unregister();
     }
 }
---
+++ tests/Doctrine/Tests/Common/ClassLoaderTest2/src1/NS/Class1.php
@@ -0,0 +1,6 @@
+<?php
+namespace NS;
+class Class1
+{
+    
+}
