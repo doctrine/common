141598a4a2c9076128905ed8008ac13dd3596b45
Benjamin Eberlei <kontakt@beberlei.de>
2011-11-18 23:18:01 +0100

    Add PHPMD @SupressWarnings annotation

--- lib/Doctrine/Common/Annotations/AnnotationReader.php
+++ lib/Doctrine/Common/Annotations/AnnotationReader.php
@@ -66,8 +66,9 @@ final class AnnotationReader implements Reader
         'abstract'=> true, 'exception'=> true, 'magic' => true, 'api' => true,
         'final'=> true, 'filesource'=> true, 'throw' => true, 'uses' => true,
         'usedby'=> true, 'private' => true, 'Annotation' => true, 'override' => true,
-        'Required' => true, 'codeCoverageIgnoreStart' => true, 'codeCoverageIgnoreEnd' => true,
-        'Target' => true, 'Attribute' => true, 'Attributes' => true, 'Required' => true,
+        'codeCoverageIgnoreStart' => true, 'codeCoverageIgnoreEnd' => true,
+        'Required' => true, 'Attribute' => true, 'Attributes' => true,
+        'Target' => true, 'SuppressWarnings' => true,
     );
 
     /**
