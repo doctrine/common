0281ab98b27d6f558a8fc3aab61a38507cfbe320
Benjamin Eberlei <kontakt@beberlei.de>
2012-01-16 13:41:15 +0100

    Bump dev version to 2.2.1

--- lib/Doctrine/Common/Version.php
+++ lib/Doctrine/Common/Version.php
@@ -36,7 +36,7 @@ class Version
     /**
      * Current Doctrine Version
      */
-    const VERSION = '2.2.0-RC5';
+    const VERSION = '2.2.1';
 
     /**
      * Compares a Doctrine version with the current one.
