0487a72d0161a32941f1086d12eff2a2ecb0076b
Daniel Anderson Tiecher <datiecher@gmail.com>
2012-02-13 22:01:48 -0200

    Added missing isInitialized method

--- lib/Doctrine/Common/Persistence/Proxy.php
+++ lib/Doctrine/Common/Persistence/Proxy.php
@@ -50,4 +50,11 @@ interface Proxy
      * @return void
      */
     public function __load();
+
+    /**
+     * Is this proxy initialized or not.
+     *
+     * @return bool
+     */
+    public function __isInitialized();
 }
