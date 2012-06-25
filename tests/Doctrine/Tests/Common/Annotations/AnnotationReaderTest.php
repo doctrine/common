cefb0d8161fac0c0d67eab5e9c78b0e37f34f839
Enrico Zimuel <e.zimuel@gmail.com>
2012-06-25 15:10:01 +0200

    Fixed the unit test for wildcard support on ignore annotation

--- tests/Doctrine/Tests/Common/Annotations/AnnotationReaderTest.php
+++ tests/Doctrine/Tests/Common/Annotations/AnnotationReaderTest.php
@@ -10,4 +10,16 @@ class AnnotationReaderTest extends AbstractReaderTest
     {
         return new AnnotationReader();
     }
+    
+    public function testSupportWildcardIgnoredAnnotation()
+    {
+        $reader = $this->getReader();
+        AnnotationReader::addGlobalIgnoredName('Foo\\*');
+        
+        $class  = new \ReflectionClass('Doctrine\Tests\Common\Annotations\Fixtures\IgnoreAnnotationWithWildcard');
+        $annots = $reader->getClassAnnotations($class);
+        
+        $this->assertEquals('foo', $annots[0]->bar);
+        $this->assertEquals('bar', $annots[1]->foo);
+    }
 }
