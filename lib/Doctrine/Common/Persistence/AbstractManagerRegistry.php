0e073d60168667524175e91a828f8a5e9949cc2a
Christophe Coevoet <stof@notk.org>
2012-07-18 20:44:25 +0200

    Added the support of namespace alias in getManagerForClass
    See doctrine/DoctrineBundle#95

--- lib/Doctrine/Common/Persistence/AbstractManagerRegistry.php
+++ lib/Doctrine/Common/Persistence/AbstractManagerRegistry.php
@@ -155,6 +155,12 @@ abstract class AbstractManagerRegistry implements ManagerRegistry
      */
     public function getManagerForClass($class)
     {
+        // Check for namespace alias
+        if (strpos($class, ':') !== false) {
+            list($namespaceAlias, $simpleClassName) = explode(':', $class);
+            $class = $this->getAliasNamespace($namespaceAlias) . '\\' . $simpleClassName;
+        }
+
         $proxyClass = new \ReflectionClass($class);
         if ($proxyClass->implementsInterface($this->proxyInterfaceName)) {
             $class = $proxyClass->getParentClass()->getName();
