d14fd323c9d8346334b8bc077287613a7113d6ef
Benjamin Eberlei <kontakt@beberlei.de>
2012-08-29 10:04:14 +0200

    Bump dev version to 2.2.4

--- lib/Doctrine/Common/Version.php
+++ lib/Doctrine/Common/Version.php
@@ -36,7 +36,7 @@ class Version
     /**
      * Current Doctrine Version
      */
-    const VERSION = '2.2.3';
+    const VERSION = '2.2.4';
 
     /**
      * Compares a Doctrine version with the current one.
