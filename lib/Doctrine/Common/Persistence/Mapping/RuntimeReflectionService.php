2cb32a62c43edc62edb91a23196cbe8743253cea
Benjamin Eberlei <kontakt@beberlei.de>
2014-02-03 23:46:02 +0100

    [DDC-2708][DCOM-219] Fix PHP 5.4.8 not returning true is_callable on abstract methods anymore.

--- lib/Doctrine/Common/Persistence/Mapping/RuntimeReflectionService.php
+++ lib/Doctrine/Common/Persistence/Mapping/RuntimeReflectionService.php
@@ -21,6 +21,9 @@ namespace Doctrine\Common\Persistence\Mapping;
 
 use ReflectionClass;
 use ReflectionProperty;
+use ReflectionMethod;
+use ReflectionException;
+
 use Doctrine\Common\Reflection\RuntimePublicReflectionProperty;
 use Doctrine\Common\Persistence\Mapping\MappingException;
 
@@ -92,6 +95,12 @@ class RuntimeReflectionService implements ReflectionService
      */
     public function hasPublicMethod($class, $method)
     {
-        return method_exists($class, $method) && is_callable(array($class, $method));
+        try {
+            $reflectionMethod = new ReflectionMethod($class, $method);
+        } catch (ReflectionException $e) {
+            return false;
+        }
+
+        return $reflectionMethod->isPublic();
     }
 }
