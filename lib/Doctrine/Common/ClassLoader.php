cc1b4e179004175fefd2f643b1e991110ec06211
Marco Pivetta <ocramius@gmail.com>
2014-12-31 12:28:24 +0100

    Non-existing files are skipped by the class loader

--- lib/Doctrine/Common/ClassLoader.php
+++ lib/Doctrine/Common/ClassLoader.php
@@ -179,6 +179,10 @@ class ClassLoader
             return false;
         }
 
+        if (! $this->canLoadClass($className)) {
+            return false;
+        }
+
         require ($this->includePath !== null ? $this->includePath . DIRECTORY_SEPARATOR : '')
                . str_replace($this->namespaceSeparator, DIRECTORY_SEPARATOR, $className)
                . $this->fileExtension;
