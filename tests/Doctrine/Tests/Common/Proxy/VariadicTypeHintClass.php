658d5ca0e43d929f5c8eab54f27fd4d057faf4af
Ruud Kamphuis <ruudk@mphuis.com>
2015-02-09 12:20:22 +0100

    Support for Variadic arguments

---
+++ tests/Doctrine/Tests/Common/Proxy/VariadicTypeHintClass.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Doctrine\Tests\Common\Proxy;
+
+/**
+ * Test asset class
+ */
+class VariadicTypeHintClass
+{
+    /**
+     * @param ...$types
+     */
+    public function addType(...$types)
+    {
+    }
+}
