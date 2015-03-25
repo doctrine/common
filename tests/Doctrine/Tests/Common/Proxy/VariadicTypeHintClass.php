d6d0007635da876e5912c18dd1a6056304154010
Marco Pivetta <ocramius@gmail.com>
2015-03-25 23:58:32 +0000

    Functional tests for variadic arguments

--- tests/Doctrine/Tests/Common/Proxy/VariadicTypeHintClass.php
+++ tests/Doctrine/Tests/Common/Proxy/VariadicTypeHintClass.php
@@ -7,10 +7,23 @@ namespace Doctrine\Tests\Common\Proxy;
  */
 class VariadicTypeHintClass
 {
+    public $types;
+    public $foo;
+    public $bar;
+    public $baz;
+
     /**
      * @param ...$types
      */
     public function addType(...$types)
     {
+        $this->types = $types;
+    }
+
+    public function addTypeWithMultipleParameters($foo, $bar, ...$baz)
+    {
+        $this->foo = $foo;
+        $this->bar = $bar;
+        $this->baz = $baz;
     }
 }
