9c6ee7f26b92bac40b0f550292a371dcb3bcd7bf
Aurelijus Rožėnas <aurelijus.rozenas@gmail.com>
2016-12-25 10:22:12 +0200

    Updated AnnotationDriverTest to test symlinked dir.

--- tests/Doctrine/Tests/Common/Persistence/Mapping/AnnotationDriverTest.php
+++ tests/Doctrine/Tests/Common/Persistence/Mapping/AnnotationDriverTest.php
@@ -6,18 +6,35 @@ use Doctrine\Common\Annotations\AnnotationReader;
 use Doctrine\Common\Persistence\Mapping\Driver\AnnotationDriver;
 use Doctrine\Common\Persistence\Mapping\ClassMetadata;
 use Doctrine\Entity;
+use Doctrine\SubDirTestClass;
+use Doctrine\SymlinkedTestClass;
 use Doctrine\TestClass;
 
 class AnnotationDriverTest extends \PHPUnit_Framework_TestCase
 {
     public function testGetAllClassNames()
     {
+        $this->makeSymlink();
+
         $reader = new AnnotationReader();
         $driver = new SimpleAnnotationDriver($reader, [__DIR__ . '/_files/annotation']);
 
         $classes = $driver->getAllClassNames();
 
-        $this->assertEquals([TestClass::class], $classes);
+        $this->assertEquals([SubDirTestClass::class, SymlinkedTestClass::class, TestClass::class], $classes);
+    }
+
+    private function makeSymlink()
+    {
+        $symlinkTarget = __DIR__ . '/../SubDirForSymlink';
+        $symlinkPath = __DIR__ . '/_files/annotation/SubDirForSymlink';
+        if (!is_link($symlinkPath)) {
+            if (file_exists($symlinkPath)) {
+                throw new \Exception(sprintf('Remove file/dir "%s".', $symlinkPath));
+            } else {
+                symlink($symlinkTarget, $symlinkPath);
+            }
+        }
     }
 }
 
