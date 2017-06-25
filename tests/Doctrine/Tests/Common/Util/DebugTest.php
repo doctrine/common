2b5ae67cdb7066a6f8740651c501638d2e597e8a
Gabriel Ostrolucký <gabriel.ostrolucky@everlution.sk>
2017-06-25 13:52:17 +0200

    Test for object with reference

--- tests/Doctrine/Tests/Common/Util/DebugTest.php
+++ tests/Doctrine/Tests/Common/Util/DebugTest.php
@@ -17,6 +17,19 @@ class DebugTest extends DoctrineTestCase
         $this->assertEquals( "stdClass", $var->__CLASS__ );
     }
 
+    public function testExportObjectWithReference()
+    {
+        $foo = 'bar';
+        $bar = ['foo' => & $foo];
+        $baz = (object) $bar;
+
+        $var = Debug::export($baz, 2);
+        $baz->foo = 'tab';
+
+        $this->assertEquals('bar', $var->foo);
+        $this->assertEquals('tab', $bar['foo']);
+    }
+
     public function testExportArray()
     {
         $array = ['a' => 'b', 'b' => ['c', 'd' => ['e', 'f']]];
