92af717f4397726c59afc690649ad7f602611211
Michael Moravec <me@majkl.me>
2018-11-21 02:42:31 +0100

    Bump version to 2.10.1-DEV

--- lib/Doctrine/Common/Version.php
+++ lib/Doctrine/Common/Version.php
@@ -18,7 +18,7 @@ class Version
     /**
      * Current Doctrine Version.
      */
-    const VERSION = '2.10.0';
+    const VERSION = '2.10.1-DEV';
 
     /**
      * Compares a Doctrine version with the current one.
